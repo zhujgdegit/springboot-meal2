00ea68b7dd2bce536218c7f3a6dd202ccc5dd886
绝尘敬尘 <1732218052@qq.com>
2024-05-09 20:55:58 +0800

    user订单查找/删除

--- src/main/java/top/naccl/controller/user/UserOrderController.java
+++ src/main/java/top/naccl/controller/user/UserOrderController.java
@@ -1,17 +1,19 @@
 package top.naccl.controller.user;
 
 
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import top.naccl.annotation.OnlyAdmin;
 import top.naccl.bean.OrderInfo;
+import top.naccl.bean.User;
 import top.naccl.service.DiningCarService;
 import top.naccl.service.OrderService;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/user")
@@ -25,7 +27,7 @@ public class UserOrderController {
     private OrderService orderService;
 
     /**
-     * 按分类查询菜品
+     * 订单详情查找
      */
     @PostMapping("/orders/detail")
     public String search(@RequestParam Integer ordderId, Model model) {
@@ -34,5 +36,39 @@ public class UserOrderController {
         model.addAttribute("orderMap", model);
 
         return "user/orderInfoCar";
+    }
+
+    /**
+     * user订单搜索
+     */
+    @PostMapping("/orders/search")
+    public String search(@RequestParam String ordCode, Model model) {
+
+        if (StringUtils.isEmpty(ordCode)) {
+            model.addAttribute("orderMap", diningCarService.getOrdersV2());
+        } else {
+            model.addAttribute("orderMap", diningCarService.getOrdersV2BYCode(ordCode));
+        }
+        return "user/orderInfo :: orderList";
+    }
+
+    /**
+     * 订单删除
+     */
+    @GetMapping("/orders/deleteByCode")
+    @ResponseBody
+    public JSONObject deleteByCode(@RequestParam String ordCode, HttpSession session) {
+
+        JSONObject result = new JSONObject();
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            result.put("success", false);
+            result.put("message", "登录已失效，请重新登录！");
+        } else {
+            orderService.deleteByCode(ordCode);
+            result.put("success", true);
+            result.put("message", "订单删除成功");
+        }
+        return result;
     }
 }
